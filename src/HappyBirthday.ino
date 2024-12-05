bf2656f99c91b95ced4b81518f4f14b8daf411d2
0xviel <ds2318014@gmail.com>
2024-12-05 08:41:19 +0700

    Create HappyBirthday.ino

---
+++ src/HappyBirthday.ino
@@ -0,0 +1,12 @@
+#include "config.h"
+#include "melody.h"
+#include "display.h"
+
+void setup() {
+    initDisplay(); // inisialisasi LCD dan tampilkan pesan
+    playMelody();  // play music Happy Birthday
+}
+
+void loop() {
+    // Tidak ada aksi di loop
+}
